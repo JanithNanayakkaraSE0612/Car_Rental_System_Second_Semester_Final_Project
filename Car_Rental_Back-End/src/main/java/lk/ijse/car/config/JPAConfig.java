e5e9fc89244ace8c76faa7576307809500030bba
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-10-24 03:01:32 +0530

    RegisterForm

--- Car_Rental_Back-End/src/main/java/lk/ijse/car/config/JPAConfig.java
+++ Car_Rental_Back-End/src/main/java/lk/ijse/car/config/JPAConfig.java
@@ -1,11 +1,14 @@
 package lk.ijse.car.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "lk.ijse.car.repo")
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
+
 }
