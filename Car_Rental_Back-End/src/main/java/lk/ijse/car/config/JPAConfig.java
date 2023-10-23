854714bd865b150c6eb75f9657da46d0f3a84235
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-10-24 02:53:24 +0530

    Create Repo packages

---
+++ Car_Rental_Back-End/src/main/java/lk/ijse/car/config/JPAConfig.java
@@ -0,0 +1,11 @@
+package lk.ijse.car.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "lk.ijse.car.repo")
+public class JPAConfig {
+}
