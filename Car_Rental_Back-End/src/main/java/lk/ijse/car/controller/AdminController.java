22a876b39b347d8881a515e1023586777f7877cd
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-10-24 18:31:15 +0530

    JPA Config TIme

--- Car_Rental_Back-End/src/main/java/lk/ijse/car/controller/AdminController.java
+++ Car_Rental_Back-End/src/main/java/lk/ijse/car/controller/AdminController.java
@@ -1,8 +1,5 @@
 package lk.ijse.car.controller;
 
-import com.easy.car_rental.dto.AdminDTO;
-import com.easy.car_rental.service.AdminService;
-import com.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
