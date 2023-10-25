7d4c9d390d41729f95fd4960954828b3be057c55
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-10-26 03:18:49 +0530

    Created Repo Package

---
+++ Car_Rental_Back-End/src/main/java/lk/ijse/car/repo/AdminRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.car.repo;
+
+import com.easy.car_rental.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepo extends JpaRepository<Admin, String> {
+}
