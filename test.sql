1f5a79301819286e730807e37b7b8d963dbec18b
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-22 02:34:34 +0530

    uPDATE

---
+++ test.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT prof.name AS "PROFESSOR.NAME", cou.name AS "COURSE.NAME"
+FROM professor prof
+INNER JOIN schedule sch
+ON sch.professor_id = prof.id
+INNER JOIN course cou
+ON sch.course_id = cou.id
+WHERE cou.department_id <> prof.department_id;
